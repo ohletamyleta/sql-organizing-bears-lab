ff57b338284992c5384737f385cf037eb0f25260
ohletamyleta <ohletamyleta@gmail.com>
2021-01-14 15:56:09 -0500

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Docile", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 11, "F", "Black", "Active", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 9, "F", "Tan", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Albino", "Chill", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "Active", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "Tan", "Antisocial", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "Brown", "Active", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL,  6, "M", "Brown", "Aggressive", 0);
